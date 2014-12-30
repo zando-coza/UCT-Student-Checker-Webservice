d9efd96b4dabdaf64d51b86678770f423c2abcfc
Alastair Hendricks <hendricks.alastair@gmail.com>
2014-12-30 12:04:54 +0200

    something's working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #UCT Student Checker Webservice
 This is a nifty webservice for communicating with the UCT Student Checker.
+
+##Getting Started
+- run bundle install in UCT-Student-Checker
--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
+require 'peoplesoft_scraper'
 require 'sinatra'
+
+set :server, 'webrick'
+
 
 get '/student/:studentnumber' do
   # Send the student number through to the existing peopleSoft parser
-  "Hello #{params[:studentnumber]}!"
+  studentNumber =params[:studentnumber]
+  begin
+    o = PeoplesoftScraper.retrieve(studentNumber)
+     "Student Name: #{o[:student_name]}"
+  rescue Exception => e
+
+  end
 end
