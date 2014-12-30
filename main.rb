170088edbc1dc643cd71e343a8d99f55a1c47019
Alastair Hendricks <hendricks.alastair@gmail.com>
2014-12-30 12:49:11 +0200

    removed submodule and got things working

--- main.rb
+++ main.rb
@@ -1,20 +1,53 @@
 require 'peoplesoft_scraper'
 require 'sinatra'
+require 'json'
 
 set :server, 'webrick'
 
 
 get '/student/:studentnumber' do
+  content_type :json
   # Send the student number through to the existing peopleSoft parser
   studentNumber =params[:studentnumber]
   begin
+    # Initialize the Object
     o = PeoplesoftScraper.retrieve(studentNumber)
-    recentYearResults = o[:terms].first
-     mostRecentYear = recentYearResults[:year]
-     puts mostRecentYear
-     "Student Name: #{o[:student_name]} #{o[:terms]}"
 
+    # Get the most recent year of results
+    recentYearResults = o[:terms].first
+
+    # Get the most recent year
+    mostRecentYear = recentYearResults[:year]
+
+    # Get the current year
+    time = Time.new
+    currentYear = time.year
+
+    # Check if the student is registered for the current year
+    if mostRecentYear == currentYear then
+
+      # set a OK status code
+      status 200
+      studentName = o[:student_name]
+
+      # build response
+      {:studentNumber => studentNumber, :studentName => studentName, :mostRecentYear => mostRecentYear}.to_json
+    else
+
+      # set status code to not found
+      status 404
+
+      # build response
+      {:studentNumber => studentNumber, :error => 'Student not registered in 2015'}.to_json
+    end
+
+      # handle exception
   rescue Exception => e
 
+    # set status code to not found
+    status 404
+
+    # build response
+    {:studentNumber => studentNumber, :error => 'Student not found'}.to_json
   end
 end
